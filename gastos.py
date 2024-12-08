294d038651bf3a3229ec54def3e1b61d2996efed
Lucas Ezequiel Gonçalves França <127765876+lucasezequielfranca@users.noreply.github.com>
2024-12-08 20:00:11 -0300

    refatorando o sistema de filtrar strings

--- gastos.py
+++ gastos.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+
+
 #filter numbers
 def filter_number(pergunta : str) -> float:
     while True:
@@ -16,38 +18,43 @@ def filter_number(pergunta : str) -> float:
 
 #filter strings
 def filter_str(pergunta: str) -> str:
+    values = list("abcdefghijklmnopqrstuvwxyz ")
+    print("O nome nao pode ser maior que 30 caracteres!\n"
+            "O nome pode conter apenas letras e numeros!")
     while True:
-        string = input(pergunta)
-        if type(string) != str:
-            print("Nome precisa ser letras!\n")
+        my_string = input(pergunta).lower()
+        if my_string == '':
+            print("Nome nao pode ficar em branco\n")
             continue
-        if string == '':
-            print("Nome nao pode ficar vazio!\n")
-            continue
+        for item in my_string:
+            if item not in values:
+                my_string = my_string.replace(item, "")
+        list1 : list = list(my_string)
+        while list1[0] == ' ':
+            list1.pop(0)
+        while list1[-1] == ' ':
+            list1.pop(-1)
+        if len(list1) > 30:
+            print("O nome nao pode ser maior que 30 caracteres\n"
+                  "Removendo caracteres excedentes!")
+        while len(list1) > 30:
+            list1.pop(-1)
+        my_string = str(''.join(list1))
         break
-    return string.lower()
+    return my_string
 
-#remove the blanck spaces in beggining and finish 
-def remove_initial_last_spaces(string : str) -> str:
-    list1 : list = list(string)
-    while list1[0] == ' ':
-        list1.pop(0)
-    while list1[-1] == ' ':
-        list1.pop(-1)
-    return str(''.join(list1))
 
 #add key to db
 def add_to_db(key : str, value : float) -> None:
-    key = remove_initial_last_spaces(key)
     temp_dict = retrieve_data_dict()
     for k in temp_dict.items():
         if key == k:
             print("Nome ja existe!\n")
             return
     try:
-        db = sqlite3.connect('db_gastos.db')
+        db = sqlite3.connect('contas.db')
         cursor = db.cursor()
-        cursor.execute(f"INSERT INTO gastos VALUES('{key}',{value})")
+        cursor.execute(f"INSERT INTO contas VALUES('{key}',{value})")
         db.commit()
         db.close()
         print("Conta adicionada com sucesso!\n")
@@ -61,9 +68,9 @@ def remove_key_from_db(target_key : str) -> None:
     for k, v in temp_dict.items():
         if target_key.lower() == k:
             try:
-                db = sqlite3.connect('db_gastos.db')
+                db = sqlite3.connect('contas.db')
                 cursor = db.cursor()
-                cursor.execute(f"DELETE from gastos WHERE conta = '{target_key.lower()}'")
+                cursor.execute(f"DELETE from contas WHERE conta = '{target_key.lower()}'")
                 db.commit()
                 db.close()
                 print("Conta removida com sucesso!\n")
@@ -77,9 +84,9 @@ def remove_key_from_db(target_key : str) -> None:
 #clear de database
 def clear_db() -> None:
     try:
-        db = sqlite3.connect('db_gastos.db')
+        db = sqlite3.connect('contas.db')
         cursor = db.cursor()
-        cursor.execute(f"DELETE from gastos")
+        cursor.execute(f"DELETE from contas")
         db.commit()
         db.close()
         print("Dados limpos com sucesso!\n")
@@ -95,10 +102,10 @@ def update_keyValue_db(target_key : str) -> None:
     for k, v in temp_dict.items():
         if k == target_key.lower():
             try:
-                new_value : float = filter_number("Digite o NOVO valor da conta: R$")
-                db = sqlite3.connect('db_gastos.db')
+                new_value : float = filter_number(f"Digite o NOVO valor de {target_key}: R$")
+                db = sqlite3.connect('contas.db')
                 cursor = db.cursor()
-                cursor.execute(f"UPDATE gastos SET valor = '{new_value}' WHERE conta = '{target_key.lower()}'")
+                cursor.execute(f"UPDATE contas SET valor = '{new_value}' WHERE conta = '{target_key.lower()}'")
                 db.commit()
                 db.close()
                 print("Valor atualizado com sucesso!\n")
@@ -106,42 +113,43 @@ def update_keyValue_db(target_key : str) -> None:
             except sqlite3.Error as erro:
                 print(f"Erro atualizando o valor: {erro}\n")
                 return
-    print("Nome nao encontrado nos dados :(\n")
+    print(f"{target_key} nao encontrado nos dados :(\n")
 
 
 #updates a key from db with the new_key value
 def update_keyName_db(target_key : str) -> None:
     temp_dict = retrieve_data_dict()
-    print(temp_dict)
 
     for k, v in temp_dict.items():
         if k == target_key.lower():
             new_key : str = filter_str("Digite o NOVO nome da conta: ")
-            try:
-                db = sqlite3.connect('db_gastos.db')
-                cursor = db.cursor()
-                cursor.execute(f"UPDATE gastos SET conta = '{new_key}' WHERE conta = '{target_key.lower()}'")
-                db.commit()
-                db.close()
-                print("Nome atualizado com sucesso!!!\n")
+            if check_size(new_key):
+                try:
+                    db = sqlite3.connect('contas.db')
+                    cursor = db.cursor()
+                    cursor.execute(f"UPDATE contas SET conta = '{new_key}' WHERE conta = '{target_key.lower()}'")
+                    db.commit()
+                    db.close()
+                    print("Nome atualizado com sucesso!!!\n")
+                    return
+                except sqlite3.Error as erro:
+                    print(f"Erro atualizando o nome: {erro}\n")
+                    return
+            else:
                 return
-            except sqlite3.Error as erro:
-                print(f"Erro atualizando o nome: {erro}\n")
-                return
-    print("Nome nao encontrado nos dados :(\n")
+    print(f"{target_key} nao encontrado nos dados :(\n")
     
 
 #retrive raw list, remeber RAW
 def retrieve_data_dict():
     temp_dict : dict[str, float] = {}
     try:
-        db = sqlite3.connect('db_gastos.db')
+        db = sqlite3.connect('contas.db')
         cursor = db.cursor()
-        keys = [conta[0] for conta in cursor.execute("SELECT conta FROM gastos")]
-        values = [valor[0] for valor in cursor.execute("SELECT valor FROM gastos")]
+        keys = [conta[0] for conta in cursor.execute("SELECT conta FROM contas")]
+        values = [valor[0] for valor in cursor.execute("SELECT valor FROM contas")]
         for i in range(0,len(keys)):
             temp_dict[keys[i]] = values[i]
-        print("Dados carrergados com sucesso!\n")
         return temp_dict
     except sqlite3.Error as erro:
         print(f"Erro carregando dados: {erro}\n")
@@ -149,12 +157,12 @@ def retrieve_data_dict():
 def print_menu():
         temp_dict = retrieve_data_dict()
         line()
-        print(" MENU DE GASTOS ".center(80, '*'))
+        print(" MENU DE contas ".center(80, '*'))
         line()
         print(f"Conta".center(39) + "|" + "Valor".center(39))
         line()
         for k, v in temp_dict.items():
-            print("-----|" + f"{k}".center(33) + "|             " + f"R${v:.02f}".ljust(21) + "|-----")
+            print("-----|" + f"{k}".rjust(30) + "   |   " + f"R${v:.02f}".ljust(31) + "|-----")
         line()
 
 def line():
@@ -166,26 +174,31 @@ def main() -> None:
         print(" 1. Cadastrar conta\n",
               "2. Atualizar nome da conta\n",
               "3. Atualizar valor da conta\n",
+              "4. Deletar conta do sistema\n",
+              "5. Remover todas as contas\n",
+
 
               )
         selection = input(">>").lower()
         match selection:
             case '1':
-                add_to_db(filter_str("Digite o nome da conta: "), filter_number("Digite o valor da conta: R$"))
+                nome : str = filter_str("Digite o nome da conta a cadastrar: ")
+                if check_size(nome):
+                    valor : float = filter_number(f"Digite o valor de {nome}: R$")
+                    add_to_db(nome, valor)
             case '2':
-                update_keyName_db(filter_str("Digite o nome da conta: "))
+                
+                update_keyName_db(filter_str("Digite o nome da conta a ser atualizado: "))
             case '3':
-                break
-
-
-
-
-
-
-
-
-
-
-add_to_db('testa', 11111111)
+                update_keyValue_db(filter_str("Digite o nome da conta para alterar o valor: "))
+            case '4':
+                nome : str = filter_str("Digite o nome da conta a ser removida: ")
+                if filter_str(f"Tem certeza que deseja remover {nome}? (S/N): ") == 's':
+                    remove_key_from_db(nome)
+            case '5':
+                if filter_str("Deseja remover todos os dados do sistema? (S/N): ") == 's':
+                    if filter_str("Tem certeza? (S/N): ") == 's':
+                        if filter_str("Todos os dados serao apagados!!! (S/N): ") == 's':
+                            clear_db()
 
 main()
