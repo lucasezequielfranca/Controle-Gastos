ba21a121a8e8185c6472493de7f6b57a4f88b542
Lucas Ezequiel Gonçalves França <127765876+lucasezequielfranca@users.noreply.github.com>
2024-12-19 14:38:41 -0300

    refatoracao do menu do dia

---
+++ funcoes.py
@@ -0,0 +1,54 @@
+from termcolor import colored as colored
+
+def filtrar_float(pergunta : str) -> float:
+    while True:
+        try:
+            value = float(input(pergunta))
+            if value <= 0:
+                print("Valor precisa ser maior que 0.01!\n")
+                continue
+            break
+        except:
+                print(f"Valor precisa ser um numero!\n")
+                continue
+    return float(value)
+
+def filtrar_int(pergunta : str) -> float:
+    while True:
+        try:
+            value = int(input(pergunta))
+            if value <= 0:
+                print("Valor precisa ser maior que 0!\n")
+                continue
+            break
+        except:
+                print(f"Valor precisa ser um numero inteiro!\n")
+                continue
+    return int(value)
+
+def filtrar_str(pergunta: str) -> str:
+    values = list("abcdefghijklmnopqrstuvwxyz ")
+    while True:
+        my_string = input(pergunta).lower()
+        if my_string == '':
+            print("Nome nao pode ficar em branco\n")
+            continue
+        for item in my_string:
+            if item not in values:
+                my_string = my_string.replace(item, "")
+        list1 : list = list(my_string)
+        while list1[0] == ' ':
+            list1.pop(0)
+        while list1[-1] == ' ':
+            list1.pop(-1)
+        if len(list1) > 30:
+            print("O nome nao pode ser maior que 30 caracteres\n"
+                  "Removendo caracteres excedentes!")
+        while len(list1) > 30:
+            list1.pop(-1)
+        my_string = str(''.join(list1))
+        break
+    return my_string
+
+def linha():
+    print('-'.center(87, '-'))
--- main.py
+++ main.py
@@ -1,17 +1,37 @@
-import contas_fixas
-from contas_fixas import line as line
+import menu_contas_fixas
+import menu_dia_detalhado
+from calendar import monthrange
+from datetime import datetime
+from funcoes import *
+import sqlite3
+import os.path
+
+current_month : int = int(datetime.today().strftime('%m'))
+current_day : int = int(datetime.today().strftime('%d'))
+current_year : int = int(datetime.today().strftime('%Y'))
+month_range_tupple : int = monthrange(current_year, current_month)
+month_range : int = int(month_range_tupple[1])
+
+
+
 print("Programa desenvolvido por Lucas Franca\n")
 print('Toda honra para Deus e Jesus Cristo\n')
 while True:
-        line()
+        linha()
         print(" Sistema de Controle de Financeiro ".center(87, '*'))
-        line()
-        print(" 1. Menu Contas Fixas\n",
-              "5. Encerrar o sistema\n"
+        linha()
+        print(" 1. Menu do Dia\n",
+              "2. Acessar outro Dia\n",
+              "3. Menu Contas fixas\n"
+              " 5. Encerrar o sistema\n"
               )
         selection = input(">>")
         match selection:
             case '1':
-                contas_fixas.main()
+                menu_dia_detalhado.main(current_day, current_month, current_year, month_range)
+            case '2':
+                pass
+            case '3':
+                  menu_contas_fixas.main()
             case '5':
                   exit()
